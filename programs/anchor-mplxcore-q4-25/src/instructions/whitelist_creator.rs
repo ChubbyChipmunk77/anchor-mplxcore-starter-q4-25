1961f14e6dd146c31dd842cc23f235990f674364
ChubbyChipmunk77 <technozap5002@gmail.com>
2026-02-09 02:05:25 +0530

    done with the mplx assignment, did read about nft core from metaplex docs.

--- programs/anchor-mplxcore-q4-25/src/instructions/whitelist_creator.rs
+++ programs/anchor-mplxcore-q4-25/src/instructions/whitelist_creator.rs
@@ -1,11 +1,16 @@
+use crate::{error::MPLXCoreError, program::AnchorMplxcoreQ425, state::WhitelistedCreators};
 use anchor_lang::prelude::*;
 
-use crate::{error::MPLXCoreError, program::AnchorMplxcoreQ425, state::WhitelistedCreators};
-
-#[derive(Accounts)] 
+#[derive(Accounts)]
+//The goal here is that this instruction should allow us
+//i.e the ones who deployed this program to add creators to the whitelist, so that only those
+//creators can be used in the minting process of the candy machine.
 pub struct WhitelistCreator<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
+
+    //UncheckedAccount means that the Account can be any type of account, and we won't be doing any
+    //checks on it in this instruction.
     /// CHECK should be a keypair
     pub creator: UncheckedAccount<'info>,
     #[account(
@@ -17,6 +22,9 @@ pub struct WhitelistCreator<'info> {
     )]
     pub whitelisted_creators: Account<'info, WhitelistedCreators>,
     pub system_program: Program<'info, System>,
+    //Here we are checking that the program data account associated with this program is the one
+    //being passed in, to ensure that only the program update authority can add creators to the
+    //whitelist.
     #[account(constraint = this_program.programdata_address()? == Some(program_data.key()))]
     pub this_program: Program<'info, AnchorMplxcoreQ425>,
     // Making sure only the program update authority can add creators to the array
@@ -26,6 +34,9 @@ pub struct WhitelistCreator<'info> {
 
 impl<'info> WhitelistCreator<'info> {
     pub fn whitelist_creator(&mut self) -> Result<()> {
+        //using the already defined function in the WhitelistedCreators struct to add the creator
+        //to the whitelist
         self.whitelisted_creators.whitelist_creator(&self.creator)
     }
 }
+
--- programs/anchor-mplxcore-q4-25/src/state/whitelisted_creators.rs
+++ programs/anchor-mplxcore-q4-25/src/state/whitelisted_creators.rs
@@ -12,18 +12,24 @@ pub struct WhitelistedCreators {
 
 impl WhitelistedCreators {
     pub fn contains(&self, creator: &AccountInfo) -> bool {
+        //[1..n] this is a good slicing technique to only check the portion of the array that has
+        //been filled with whitelisted creators, instead of checking the entire array which may
+        //contain default values (Pubkey::default()) that could match the creator's key and give a
+        //false positive.
         self.creators[..self.num_creators as usize].contains(creator.key)
     }
 
     pub fn whitelist_creator(&mut self, creator: &AccountInfo) -> Result<()> {
         // Check if the array is full
+        // This error macro here is provided by Anchor and allows us to return a custom error
+        // defined in our MPLXCoreError enum.
         if self.num_creators as usize >= self.creators.len() {
             return err!(MPLXCoreError::CreatorListFull);
         }
 
         // Check if already whitelisted
         if self.contains(creator) {
-            return err!(MPLXCoreError::CreatorAlreadyWhitelisted)
+            return err!(MPLXCoreError::CreatorAlreadyWhitelisted);
         }
 
         // Add the creator at the current num_creators index
